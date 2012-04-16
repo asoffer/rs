f17e8e3231d4d4a6327ab677dcda49e4927052e6
Andy <andy@s231-225.resnet.ucla.edu>
2012-04-16 08:12:34 -0700

    added input from main page

--- js/main.js
+++ js/main.js
@@ -1,43 +1,28 @@
 $(document).ready(function(){
-/*
-    tab = new Tableaux();
-    ta2 = new Tableaux();
-
-    tab.insert(4);
-    ta2.place(1, tab);
-
-    tab.insert(2);
-    ta2.place(2, tab);
-
-    tab.insert(3);
-    ta2.place(3, tab);
-
-    tab.insert(6);
-    ta2.place(4, tab);
-
-    tab.insert(5);
-    ta2.place(5, tab);
-
-    tab.insert(1);
-    ta2.place(6, tab);
-
-    tab.insert(7);
-    ta2.place(7, tab);
-
-    $("#content").html(""+tab);
-    $("#content").append("<br><br>"+ta2);
-*/
     var l = new List();
-    l.pushBack(4);
-    l.pushBack(2);
     l.pushBack(3);
-    l.pushBack(6);
-    l.pushBack(5);
+    l.pushBack(4);
     l.pushBack(1);
-    l.pushBack(7);
+    l.pushBack(2);
 
     var x = Tableaux.robinsonSchensted(l);
-    console.log(x);
+
     $("#content").html("" + x.first);
     $("#content").append("<br><br>" + x.second);
 });
+
+function doRS(){
+    var str = $("#perm").val();
+
+    var a = str.split(",");
+    var l = new List();
+
+    for(var i = 0; i < a.length; ++i){
+        l.pushBack(parseInt(a[i]));
+    }
+
+    x = Tableaux.robinsonSchensted(l);
+    $("#content").html("" + x.first);
+    $("#content").append("<br><br>" + x.second);
+
+}
