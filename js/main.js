e874ec32fc2beedc666b8112c09ef4bdcdeaa824
Andy <andy@s231-225.resnet.ucla.edu>
2012-04-16 09:00:41 -0700

    css to make it pretty. copied from nomsg

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
+var GLOBAL_v = 0.9;
+var GLOBAL_d = "Apr 16, 2012";
+
 $(document).ready(function(){
-    var l = new List();
-    l.pushBack(3);
-    l.pushBack(4);
-    l.pushBack(1);
-    l.pushBack(2);
+    $("#version").html("<span id = \"vers\">Version " + GLOBAL_v + "</span><br>Last updated "+GLOBAL_d);
+    //$("#vers").click(function(){
+    //sendMessage("Changes","<h4>What's new in Version " + GLOBAL_v + "?</h4><ul><li>Groups sizes $540, 630, 810, 990, 1890$ and more solved.</li></ul><h4>What's new in Version 1.2?</h4><ul><li>Display issues with long lists fixed.</li><li>Potentially long lines only displayed at users choice.</li><li>Added capability to input arithmetic expressions.</li></ul>");
+    //});
 
-    var x = Tableaux.robinsonSchensted(l);
+    $("#in").keyup(function(e){
+        if(e.keyCode == 13)
+        $("#go").click();
+    });
 
-    $("#content").html("" + x.first);
-    $("#content").append("<br><br>" + x.second);
+
+    $("#go")
+    .button()
+    .click(doRS);
 });
 
 function doRS(){
-    var str = $("#perm").val();
+    var str = $("#in").val();
 
     var a = str.split(",");
     var l = new List();
@@ -22,7 +29,27 @@ function doRS(){
     }
 
     x = Tableaux.robinsonSchensted(l);
-    $("#content").html("" + x.first);
-    $("#content").append("<br><br>" + x.second);
+    $("#yt_left").html("" + x.first);
+    $("#yt_right").html("" + x.second);
 
+}
+
+function sendMessage(title, message){
+    $("#message").html(message);
+    setDialog(title);
+    $("#message").dialog("open");
+}
+
+function setDialog(title){
+    $("#message").dialog({
+        width: 400,
+    resizable: false,
+    autoOpen: false,
+    modal: true,
+    open: function(){
+        $('.ui-widget-overlay').hide().fadeIn();
+    },
+    hide: "fade",
+    title: title
+    });
 }
